32a1b5a78f725e4766bcdf7fed89aeff8c945b52
Yaapa Hage <hage.yaapa@in.ibm.com>
2020-07-30 22:30:06 +0530

    chore: discuss defineModelClass and getModelSchemaRef issue
    Discuss getModelSchemaRef
    
    Signed-off-by: Yaapa Hage <hage.yaapa@in.ibm.com>

--- packages/rest-crud/src/__tests__/unit/crud-rest-controller.unit.ts
+++ packages/rest-crud/src/__tests__/unit/crud-rest-controller.unit.ts
@@ -3,9 +3,14 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {Entity, model, property} from '@loopback/repository';
-import {expect} from '@loopback/testlab';
-import {defineCrudRestController} from '../..';
+import {
+  defineModelClass,
+  Entity,
+  model,
+  ModelDefinition,
+  property,
+} from '@loopback/repository';
+import {getModelSchemaRef} from '@loopback/rest';
 
 describe('defineCrudRestController', () => {
   it('should generate controller based on Model name', async () => {
@@ -15,12 +20,40 @@ describe('defineCrudRestController', () => {
       id: number;
     }
 
-    const CrudRestController = defineCrudRestController<
-      Product,
-      typeof Product.prototype.id,
-      'id'
-    >(Product, {basePath: '/products'});
+    const srA = getModelSchemaRef(Product);
+    console.log(srA); // <-- full schema ref
 
-    expect(CrudRestController.name).to.equal('ProductController');
+    // const CrudRestControllerA = defineCrudRestController<
+    //   Product,
+    //   typeof Product.prototype.id,
+    //   'id'
+    // >(Product, {basePath: '/products'});
+
+    // -----------------------------------
+
+    const bookDef = new ModelDefinition({
+      name: 'Book',
+      properties: {
+        id: {
+          type: 'Number',
+          required: true,
+        },
+      },
+    });
+
+    const Book = defineModelClass<typeof Entity, {id: number}>(Entity, bookDef);
+
+    const srB = getModelSchemaRef(Book);
+    console.log(srB); // <-- {}
+
+    // const CrudRestControllerB = defineCrudRestController<
+    //   Book, // <-- Book is not a "real" Class
+    //   typeof Book.prototype.id,
+    //   'id'
+    // >(Book, {basePath: '/books'});
+
+    // const CrudRestControllerB = defineCrudRestController(Book, {
+    //   basePath: '/books',
+    // });
   });
 });
